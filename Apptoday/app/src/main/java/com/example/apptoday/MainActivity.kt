0f50c69107ea39c875b836ba0447358d0d22d959
walasJunior <88062443+walasJunior@users.noreply.github.com>
2023-04-12 20:48:46 -0300

    TaskTodayApp

--- Apptoday/app/src/main/java/com/example/apptoday/MainActivity.kt
+++ Apptoday/app/src/main/java/com/example/apptoday/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.example.apptoday
 
 import android.os.Bundle
+import android.telecom.Call.Details
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.apptoday.ui.theme.ApptodayTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +39,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun MainScreenContest (){
+fun MainScreenContest (drawerState: DrawerState){
+    val scaffoldState = rememberScaffoldState(drawerState = drawerState)
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             TopAppBar(
-                content =  { Text(text = "taskAppBar")}
+                title = { Text(text = "TaskTodayAPP")},
+                navigationIcon = {
+                    IconButton(onClick = {
+                        CoroutineScope(Dispatchers.Default).launch {
+                            scaffoldState.drawerState.open()
+                        }
+                         }
+                    ) {
+
+                        Icon(
+
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Drawer Menu")
+
+                }
+        }
             )
 
         },
@@ -45,7 +73,18 @@ fun MainScreenContest (){
 
             ){
                 MySearchField(modificador = Modifier.fillMaxWidth())
-                MyTaskWidget(modificador = Modifier.fillMaxWidth() )
+                MyTaskWidget(
+                    modificador = Modifier.fillMaxWidth(),
+                    taskName = "Preparar Aula LazyList/LazyColum",
+                    taskDetails ="É bem melhor usar lazilist ao inves de colum",
+                    deadEndDate = Date()
+                )
+                MyTaskWidget(
+                    modificador = Modifier.fillMaxWidth(),
+                    taskName = "Prova Matematica",
+                    taskDetails ="Estudar Calculo capitulo 1 e 2" ,
+                    deadEndDate = Date() )
+
                 Text("Task3")
                 Text("Task4")
 
@@ -80,13 +119,48 @@ fun MySearchField(modificador: Modifier){
         )
 }
 @Composable
-fun MyTaskWidget(modificador: Modifier){
-    Column(modifier = modificador) {
-        Text(text = "Tarefa X")
+fun MyTaskWidget
+            (modificador: Modifier,
+             taskName : String,
+             taskDetails: String,
+             deadEndDate: Date
+            ){
+    val dateFormatter = SimpleDateFormat("eee,mmm,dd,yyyyy", Locale.getDefault())
+    Row(modifier = modificador) {
+        Column() {
+            
 
+        Icon(
+            imageVector = Icons.Default.Notifications,
+            contentDescription = "Icons of a pedent task"
+        )
+        Text(text = dateFormatter.format(deadEndDate),
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        fontSize = 12.sp
+        )
+    }
+    Column(
+        modifier = modificador
+            .border(width = 1.dp, color = Color.Black)
+            .padding(3.dp)
+    ) {
+        Text(
+            text = taskName,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = taskDetails,
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
+        )
     }
 }
-
+    Spacer(modifier = Modifier.height(16.dp))
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
